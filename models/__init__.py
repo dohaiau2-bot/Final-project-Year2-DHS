3ab97cbd40ec83923f57811d7a4ece24a4655c1e
dohaiau2 <dohaiau2@gmail.com>
2026-05-31 23:05:29 +0700

    added init for Correct cross-imports

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+
+from .product import Product, Laptop, Phone, Fridge
+
+# Định nghĩa những class được phép xuất xưởng ra ngoài khi dùng lệnh import *
+__all__ = ['Product', 'Laptop', 'Phone', 'Fridge']
